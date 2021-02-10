a2d9c9195395a379add9ce92372b81d5b7eefc0f
John McDonnell <john@renderlogic.com>
2021-02-09 22:50:28 -0500

    RL-DOCKER-2: get basic docker install/init script working for fedora... todo: add conditions to cover different development operating system repos (i.e. ubuntu) and server operating systems... PULL Centos, RHEL, Debian etc

---
+++ init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#this script will do everything needed to get the docker renderlogic developer container installed and running.
+#this shell script was authored by john@renderlogic.com - feel free to contact me for questions and suggestions.
+
+#global constants / variables
+FEDORA_DOCKER_REPO='https://download.docker.com/linux/fedora/docker-ce.repo'
+
+#colors
+GREENFONT='\033[0;32m'
+REDFONT='\033[0;31m'
+NC='\033[0m' # No Color
+
+# first remove all old docker instances and install latest docker version following the docker for fedora guideline found 
+# here : https://docs.docker.com/engine/install/fedora/
+sudo echo "Updating System..."
+sudo dnf -y update
+sudo echo "Checking for and removing previous non-compatible docker installations... "
+sudo dnf -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+echo "Adding DNF Plugins Core..."
+sudo dnf -y install dnf-plugins-core
+echo "Installing Docker's Fedora Repository..."
+sudo dnf config-manager --add-repo $FEDORA_DOCKER_REPO
+echo "Enable Fedora Docker CE Nightly Build Repository? (yes/no)" 
+read user_answer
+if [[ $user_answer -eq 'yes' ]]; then
+	sudo dnf config-manager --set-enabled docker-ce-nightly
+	echo -e "${GREENFONT}Docker CE Nightly Enabled!${NC}"
+fi
+echo "Enable Fedora Docker CE Test Build Repository? (yes/no)" 
+read user_answer
+if [[ $user_answer -eq 'yes' ]]; then
+	sudo dnf config-manager --set-enabled docker-ce-test
+	echo -e "${GREENFONT}Docker CE Test Enabled!${NC}"
+fi
+
+#install LATEST docker engine
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
+echo "Starting Docker and Running Docker Hello World Container Test..."
+sudo systemctl start docker
+sudo docker run hello-world
