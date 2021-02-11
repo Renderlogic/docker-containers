da830b532d69b39bb18a75167e2548ae38b598f7
John McDonnell <john@renderlogic.com>
2021-02-11 08:55:50 -0500

    RL-DOCKER-7: Modified Dockerfile

--- fedora-centos-base/init.sh
+++ fedora-centos-base/init.sh
@@ -45,20 +45,4 @@ sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSI
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 sudo mkdir c7-systemd-base
-sudo touch c7-systemd-base/Dockerfile >> 
-"
-FROM centos:7
-ENV container docker
-RUN (cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == \
-systemd-tmpfiles-setup.service ] || rm -f $i; done); \
-rm -f /lib/systemd/system/multi-user.target.wants/*;\
-rm -f /etc/systemd/system/*.wants/*;\
-rm -f /lib/systemd/system/local-fs.target.wants/*; \
-rm -f /lib/systemd/system/sockets.target.wants/*udev*; \
-rm -f /lib/systemd/system/sockets.target.wants/*initctl*; \
-rm -f /lib/systemd/system/basic.target.wants/*;\
-rm -f /lib/systemd/system/anaconda.target.wants/*;
-VOLUME [ "/sys/fs/cgroup" ]
-CMD ["/usr/sbin/init"]
-"
 sudo docker build --rm -t c7-systemd-base .
